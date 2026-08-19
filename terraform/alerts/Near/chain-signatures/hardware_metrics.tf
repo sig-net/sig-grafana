50f83e778f81f201250dfc38b704d790f32e7f82
auto-mausx <auto.mausx@gmail.com>
2026-08-18 19:47:20 -0700

    fix(alerts): pause dev node memory alert

--- terraform/alerts/Near/chain-signatures/hardware_metrics.tf
+++ terraform/alerts/Near/chain-signatures/hardware_metrics.tf
@@ -51,7 +51,7 @@ resource "grafana_rule_group" "rule_group_d2d0b898365eeb0c" {
       description      = "CPU usage percentage has been at or above 90% for more than 5 mins: {{ $values.A }}%"
       summary          = "[DEV][HARDWARE] Node CPU usage 90% or above"
     }
-    is_paused = false
+    is_paused = true
 
     notification_settings {
       contact_point = "Multichain Dev"
