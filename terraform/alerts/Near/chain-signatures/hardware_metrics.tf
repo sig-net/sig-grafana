44699ee130851e017cadd35c7bcb7595845f855a
auto-mausx <auto.mausx@gmail.com>
2026-08-18 19:48:18 -0700

    fix(alerts): disable dev node memory alert

--- terraform/alerts/Near/chain-signatures/hardware_metrics.tf
+++ terraform/alerts/Near/chain-signatures/hardware_metrics.tf
@@ -106,7 +106,7 @@ resource "grafana_rule_group" "rule_group_d2d0b898365eeb0c" {
       description      = "CPU usage percentage has been at or above 90% for more than 5 mins: {{ $values.A }}%"
       summary          = "[MAINNET][HARDWARE] Node CPU usage 90% or above"
     }
-    is_paused = true
+    is_paused = false
 
     notification_settings {
       contact_point = "SRE On-call"
@@ -271,7 +271,7 @@ resource "grafana_rule_group" "rule_group_d2d0b898365eeb0c" {
       description      = "Node has abnormally high memory usage: {{ $values.A }} GB remaining"
       summary          = "[DEV][HARDWARE]Node memory usage high"
     }
-    is_paused = false
+    is_paused = true
 
     notification_settings {
       contact_point = "Multichain Dev"
